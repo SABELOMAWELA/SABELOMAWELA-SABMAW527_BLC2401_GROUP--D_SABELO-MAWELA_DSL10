ea615c2fd55575c359cd4d7904b3d675bd3c1c83
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-06-14 15:04:51 +0200

    "Added CSS rule for text alignment in .card class and implemented App component with API call to fetch posts"

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+          return response.json();
+        }
+      })
+      .then(data => setPosts(data))
+      .catch(error => setError(error.message));
+  }, []);
+
+  if (error) {
+    return {error}
+  }
+  return (
+    <div className='card'>
+      <h1>blogPost</h1>
+      <ul>
+{posts.map((post,index) => (
+  <dd key={post.id} >
+    <h2>{index + 1}.{post.title}</h2>
+    <p>{post.body}</p>
+  </dd>
+))}
+</ul>
+      
+    </div>
+  );
+}
+
+export default App;
